b04c3a5b68d66d098c629cf9cdb7fa66b8354e6c
いまいだいき <imaidaiki@imaidaikinoMBP.localdomain>
2021-03-31 14:20:20 +0900

    [change] React Routerの記述内容変更

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react'
 import './Styles/App.scss';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Header from './Organisms/Header'
 import King from './Pages/King'
 import Emoji from './Pages/Emoji'
 import Dashboard from './Pages/Dashboard'
 
-
-function App() {
-        return(
-            <div className="App">
-                <Router>
-                    <div>
-                        <Header />
-                        <Route exact path="/" component={Dashboard}/>
-                        <Route parh="/King" component={King}/>
-                        <Route parh="/Emoji" component={Emoji}/>
-                    </div>
-                </Router>
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <Header />
+                <Switch>
+                    <Route exact path="/">
+                        <Dashboard />
+                    </Route>
+                    <Route path="/Emoji">
+                        <Emoji />
+                    </Route>
+                    <Route path="/King">
+                        <King />
+                    </Route>
+                </Switch>
             </div>
-        )
-    }
-
-export default App;
+        </Router>
+    )
+}
